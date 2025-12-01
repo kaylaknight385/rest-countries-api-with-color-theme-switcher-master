c3a03e3960f13f81fb38bb2b50e0d019ee8ebe76
kaylaknight385 <kaylaknight385@gmail.com>
2025-11-30 22:34:16 -0500

    working on this again...got food posionoing..i wanna die...

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,15 +1,15 @@
-//theme switch POPPOPPOP
+//theme switch POPPOPPOP-gets elements from the html
 const themeToggle = document.getElementById('theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 const themeText = document.getElementById('theme-text');
 const html = document.documentElement;
 
-//save current theme so everytime u get on page its that theme ooooor default to light
+//save current theme to local storage everytime u get on page ooooor default to light
 let currentTheme = localStorage.getItem('theme') || 'light';
 html.setAttribute('data-theme', currentTheme);
 updateThemeButton(currentTheme);
 
-//toggle click handler
+//when button is clicked, it will cylce thru the themes
 themeToggle.addEventListener('click', () => {
     //it gon cycle thru light-dark-bunny-light
     if (currentTheme === 'light') {
@@ -21,11 +21,21 @@ themeToggle.addEventListener('click', () => {
     }
 
     html.setAttribute(data-theme, currentTheme);
+    //jus saves to local storage 
     localStorage.setItem('theme', currentTheme,)
+    //updates the button icon/text
     updateThemeButton(currentTheme)
-
 });
 
 function updateThemeButton(theme) {
-    if (theme === 'light')
+    if (theme === 'light') {
+        themeIcon.textContent = '&#127769;';
+        themeText.textContent = 'Dark Mode';
+    } else if (theme === 'dark') {
+        themeIcon.textContent = '&#128048;';
+        themeText.textContent = 'Bunny Mode'
+    } else {
+        themeIcon.textContent = '&#9728;';
+        themeText.textContent = 'Light Mode'
+    }
 }
